5299230551c871a822cd62cc5a15b7b6a9830e56
rahulgubili <rahul.gubiligari@paypay-corp.co.jp>
2024-06-12 22:29:38 +0900

    Add check API

--- src/main/kotlin/com/techie/ebanking/users/config/SecurityConfig.kt
+++ src/main/kotlin/com/techie/ebanking/users/config/SecurityConfig.kt
@@ -2,6 +2,7 @@ package com.techie.ebanking.users.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -9,6 +10,7 @@ import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalAuthentication
 class SecurityConfig {
 
     @Bean
